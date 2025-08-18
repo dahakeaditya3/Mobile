7dba1fe8b989db47ebd11f3a124de9700e9f0406
Aditya Dahake <adityadahake2002@gmail.com>
2025-08-18 09:10:21 +0530

    home page update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
+  selector: 'app-products',
+  standalone: true,  // <--- important
+  imports: [CommonModule],  // <--- this enables *ngFor, *ngIf
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+  showAll: boolean = false;
 
+  constructor(private productService: AuthService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  toggleView() {
+    this.showAll = !this.showAll;
+  }
 }
